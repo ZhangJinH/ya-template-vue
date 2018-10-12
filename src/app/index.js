abb5963e787e646e1f0f5d2ec9a6ab2ad342994f
q13 <qsimon13@gmail.com>
2018-10-12 16:33:18 +0800

    Add demo

--- src/app/index.js
+++ src/app/index.js
@@ -3,18 +3,17 @@
  */
 import hook from '+/deps/hook';
 import {
-  setAppData
+  setAppData,
+  c2s
 } from '@/deps/utils';
 import template from './template.html';
 import '../widgets/themes/ya/index.styl';
-import errorCode from '../deps/error-code';
 
 // 数据挂钩
 hook('prepare@app', function () {
   return {
     appData: null,
-    routerOptions: {},
-    errorCode
+    routerOptions: {}
   };
 });
 // App component挂钩，将component configs传递给resolve完成初始化
@@ -37,6 +36,12 @@ hook('create@app', function (options) {
     template,
     mounted() {
       console.log('route', this.$route);
+      c2s({
+        url: 'test',
+        onSuccess(result) {
+          console.log('c2s', result);
+        }
+      });
     }
   };
 });
